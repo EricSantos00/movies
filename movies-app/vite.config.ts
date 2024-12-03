e431dd2d7974742cc7e0b5ff24ccf2c2b3273a8e
Eric Santos <eric_santos2009@hotmail.com>
2024-12-03 20:04:19 -0300

    Add docker support

--- movies-app/vite.config.ts
+++ movies-app/vite.config.ts
@@ -1,12 +1,22 @@
 import react from "@vitejs/plugin-react"
 import path from "path"
 import { defineConfig } from "vite"
- 
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
   },
 })
